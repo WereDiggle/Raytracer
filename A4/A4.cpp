14f96a9c918cac977db88a68b65544b2bf94c3b6
Mingrui Zou <m2zou@uwaterloo.ca>
2017-11-05 20:20:14 -0500

    fixed stupid camera-to-world matrix. It should have been the transpose. Don't trust the matrices on the glm website, they liegit add .git add .!

--- A4/A4.cpp
+++ A4/A4.cpp
@@ -59,25 +59,68 @@ void A4_Render(
 	double xFactor = aspectRatio * fovFactor;
 
 	// TODO: Assume camera pointing directly down z axis for now
-	// TODO: get left vec by cross product of up vector and direction of view vector
-	glm::vec3 viewDirection = glm::normalize(view - eye);
-	glm::vec3 left = glm::cross(up, viewDirection);
+	// TODO: get side vec by cross product of up vector and direction of view vector
+	glm::vec3 viewDirection = view - eye;
+	glm::vec3 side = glm::cross(viewDirection, up);
+	glm::vec3 newUp = glm::cross(side, viewDirection);
+
+	std::cout << "side: " << glm::to_string(side) << std::endl;
+	std::cout << "newUp: " << glm::to_string(newUp) << std::endl;
+	std::cout << "viewDirection: " << glm::to_string(viewDirection) << std::endl;
+
+	viewDirection = glm::normalize(viewDirection);
+	side = glm::normalize(side);
+	newUp = glm::normalize(newUp);
+
+	std::cout << "side: " << glm::to_string(side) << std::endl;
+	std::cout << "newUp: " << glm::to_string(newUp) << std::endl;
+	std::cout << "viewDirection: " << glm::to_string(viewDirection) << std::endl;
+
+	// TODO: construct a camera-to-world transformation matrix, so that the camera can look from any angle
+	glm::mat4 cameraToWorldMat = glm::mat4(1);
+	// Remeber, it's [col][row]
+	// Set translation 
+	cameraToWorldMat[3][0] = eye.x;
+	cameraToWorldMat[3][1] = eye.y;
+	cameraToWorldMat[3][2] = eye.z;
+
+	// Set rotation
+	// side
+	cameraToWorldMat[0][0] = side.x;
+	cameraToWorldMat[0][1] = side.y;
+	cameraToWorldMat[0][2] = side.z;
+
+	// up
+	cameraToWorldMat[1][0] = newUp.x;
+	cameraToWorldMat[1][1] = newUp.y;
+	cameraToWorldMat[1][2] = newUp.z;
+
+	// view direction
+	cameraToWorldMat[2][0] = viewDirection.x;
+	cameraToWorldMat[2][1] = viewDirection.y;
+	cameraToWorldMat[2][2] = viewDirection.z;
+
+	std::cout << "cameraToWorld: " << glm::to_string(cameraToWorldMat) << std::endl;
+
 
 	for (int y = 0; y < imageHeight; ++y) {
 		for (int x = 0; x < imageWidth; ++x) {
 
 			// Transform the pixel in raster space to a pixel in screen space
-			//glm::vec3 pixelLoc = view + (x - imageWidth/2)*left + (y - imageHeight/2)*up;
-			glm::vec3 pixelLocation = glm::vec3( (2.0*((x+0.5)/imageWidth)-1.0) * xFactor , (1.0-2.0*(y+0.5)/imageHeight) * fovFactor, eye.z - 1/*TODO: generalize for not looking down z axis*/ );
+			//glm::vec3 pixelLoc = view + (x - imageWidth/2)*side + (y - imageHeight/2)*up;
+			// TODO: convert raster calculations into a raster-to-camera matrix
+			glm::vec3 pixelLocation = glm::vec3( (2.0*((x+0.5)/imageWidth)-1.0) * xFactor , (1.0-2.0*(y+0.5)/imageHeight) * fovFactor, 1/*TODO: generalize for not looking down z axis*/ );
+
+
+			pixelLocation = glm::vec3(cameraToWorldMat * glm::vec4(pixelLocation, 1));
+			if (y == imageHeight/2 && x == imageWidth/2) {
+				std::cout << glm::to_string(pixelLocation) << std::endl;
+			}
 			Ray primRay = Ray(eye, pixelLocation);
 			//std::cout << "x - w/2 " << (w/2) << std::endl;
 			//std::cout << "y: " << y << ", x: " << x << " " << primRay.direction.x << " " << primRay.direction.y << " " << primRay.direction.z << std::endl;
 
 			// Check for intersection for primary ray			
-
-			image(x, y, 0) = 0;
-			image(x, y, 1) = 0;
-			image(x, y, 2) = 0;
 			Intersect primRayIntersect = Intersect();
 			for (SceneNode * childNode : root->children) {
 				Intersect curRayIntersect = childNode->checkIntersection(primRay);
@@ -91,12 +134,19 @@ void A4_Render(
 			image(x, y, 1) = primRayIntersect.colour.g;
 			image(x, y, 2) = primRayIntersect.colour.b;
 
+			// TODO: remove. make a white border and cross hairs
+			if (y == 0 || y == imageHeight/2 || y == imageHeight-1 || x == 0 || x == imageWidth/2 || x == imageWidth-1) {
+				image(x, y, 0) = 1;
+				image(x, y, 1) = 1;
+				image(x, y, 2) = 1;
+			}
+
 			/*
 			// Red: increasing from top to bottom
 			image(x, y, 0) = (double)y / h;
-			// Green: increasing from left to right
+			// Green: increasing from side to right
 			image(x, y, 1) = (double)x / w;
-			// Blue: in lower-left and upper-right corners
+			// Blue: in lower-side and upper-right corners
 			image(x, y, 2) = ((y < h/2 && x < w/2)
 						  || (y >= h/2 && x >= w/2)) ? 1.0 : 0.0;
 			*/
